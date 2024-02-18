746754b5c4e438a85c8e9a8de74ec7620126f494
“SufficientDaikon” <tahaa755@yahoo.com>
2024-02-18 18:14:18 +0200

    fixed syntax errors

--- store/models.py
+++ store/models.py
@@ -5,14 +5,18 @@ class Promotion(models.Model):
     description = models.CharField(max_length=255)
     disccount = models.FloatField()
 
+
 class Collection(models.Model):
     title = models.CharField(max_length=255)
+    featured_product = models.ForeignKey(
+        "Product", null=True, on_delete=models.SET_NULL, related_name="+")
 
 
 class Product(models.Model):
     title = models.CharField(max_length=255)
+    slug = models.SlugField()
     description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
@@ -30,7 +34,7 @@ class Customer(models.Model):
     ]
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
-    emial = models.EmailField(max_length=255, uniqe=True)
+    emial = models.EmailField(max_length=255, unique=True)
     phone = models.CharField(max_length=20)
     birth_date = models.DateField(null=True)
 
@@ -48,27 +52,28 @@ class Order(models.Model):
     placed_at = models.DateTimeField(auto_now_add=True)
     payment_status = models.CharField(
         max_length=1, choices=PAYMENT_STATUS_CHOICE, default=PAYMENT_STATUS_PENDING)
-    customer = models.models.ForeignKey(on_delete=models.PROTECT)
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
 
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.PROTECT)
     product = models.ForeignKey(Product, on_delete=models.PROTECT)
     quantity = models.PositiveSmallIntegerField()
-    unit_price = models.DecimalField(max_digits=6, decial_places=2)
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
 
 
-class Address(models.model):
+class Address(models.Model):
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
+    zip = models.CharField(max_length=20, null=True)
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
 
 
-class Cart(models.model):
+class Cart(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
 
-class CartItem(models.model):
+class CartItem(models.Model):
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveSmallIntegerField()
